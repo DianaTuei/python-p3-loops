962e18f4e8532b5743937b13469e8b7a5b1bc41a
DianaTuei <memotuei@gmail.com>
2024-03-01 12:57:06 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i > 0):
+        print (i)
+        i -= 1
+    print ("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   integers_square = list()
+   for integer in int_list:
+       integer_square = integer * integer
+       integers_square.append(integer_square)
+   return integers_square
+square_integers([1,2,3,4,5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 0
+    while i < 100:
+        i += 1
+        if(i%3 == 0 and i%5 == 0):
+            print ("FizzBuzz")
+        elif(i%5 == 0):
+            print ("Buzz")
+        elif(i%3 == 0):
+            print("Fizz")
+        else:
+            print (i)
+fizzbuzz()
